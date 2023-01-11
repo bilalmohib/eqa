a777fb501f36b0c307356c87c64438bc47d22eee
Muhammad-Bilal-7896 <2019cs682@student.uet.edu.pk>
2023-01-12 00:24:49 +0500

    Responsiveness for Header Done

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -9,11 +9,11 @@ const Header = () => {
 
     useEffect(() => {
         // The debounce function receives our function as a parameter
-        const debounce = (fn:any) => {
+        const debounce = (fn: any) => {
             // This holds the requestAnimationFrame reference, so we can cancel it if we wish
-            let frame : any;
+            let frame: any;
             // The debounce function returns a new function that can receive a variable number of arguments
-            return (...params:any) => {
+            return (...params: any) => {
                 // If the frame variable has been defined, clear it now, and queue for next frame
                 if (frame) {
                     cancelAnimationFrame(frame);
@@ -50,22 +50,35 @@ const Header = () => {
                     title="EQA University"
                 />
             </div>
-            <div className={styles.mobileDropStyle}>
-                <div className="dropdown" title="Select a language for the site">
-                    <div className={`${styles.langDropDown}`} id="dropdownMenuButton" data-mdb-toggle="dropdown" aria-expanded="false">
-                        <img
-                            width={25}
-                            height={25}
-                            src={eng}
-                            alt="English"
-                            title="English"
-                        />
-                        <p>Eng <i className="fas fa-chevron-down"></i></p>
+            <div className={`d-flex ${styles.mobileDropStyle}`}>
+                <div>
+                    <div className="dropdown" title="Select a language for the site">
+                        <div className={`${styles.langDropDown}`} id="dropdownMenuButton" data-mdb-toggle="dropdown" aria-expanded="false">
+                            <img
+                                width={25}
+                                height={25}
+                                src={eng}
+                                alt="English"
+                                title="English"
+                            />
+                            <p>Eng <i className="fas fa-chevron-down"></i></p>
+                        </div>
+                        <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <li><a className="dropdown-item" href="#">About Us</a></li>
+                            <li><a className="dropdown-item" href="#">Notice Board</a></li>
+                        </ul>
                     </div>
-                    <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                        <li><a className="dropdown-item" href="#">About Us</a></li>
-                        <li><a className="dropdown-item" href="#">Notice Board</a></li>
-                    </ul>
+                </div>
+                <div>
+                    <div className="dropdown" title="Select a language for the site">
+                        <div className={`${styles.menuDropDown}`} id="dropdownMenuButton" data-mdb-toggle="dropdown" aria-expanded="false">
+                            <i className="fas fa-bars"></i>
+                        </div>
+                        <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <li><a className="dropdown-item" href="#">About Us</a></li>
+                            <li><a className="dropdown-item" href="#">Notice Board</a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </header>
